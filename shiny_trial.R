d24877971abc4d56cc71f1b1ca5ccc95ed27b37b
Jeneen Hadj-Hammou <jeneenhh@gmail.com>
2018-11-27 14:45:36 +0000

    trying to get the app online

--- shiny_trial.R
+++ shiny_trial.R
@@ -1,18 +1,32 @@
+library(tidyverse)
+library(ggrepel)
+library(funk)
+
+dat<-read.csv(file = 'frack-clean.csv')
+
 library(shiny)
+library(plotly)
+library(gapminder)
 library(ggplot2)
 
+t<- dat %>% filter(year > 1989) %>%
+  group_by(year,  region) %>%
+  summarise(q = length(ML)) %>% ungroup() %>%
+  group_by(region) %>% mutate(qq=cumsum(q)) 
+anchor <- expand.grid(region = unique(t$region), year = 1989, qq = 0, q = 0)
+t <- rbind(anchor, data.frame(t))
+
+labs <- t %>% filter(region != '') %>% group_by(region) %>% summarise(max = max(qq))
 
 
 #### selects one graph at a time and highlights in red
 ui <- fluidPage(
-              selectInput("region", "region", choices = unique(t$region)),
+              selectInput("region", "Region", choices = unique(t$region)),
               plotOutput("outplot"))
-            
-            
   
 server <- function(input, output) {
   
-  output$outplot <- renderPlot({
+  output$outplot <- renderPlotly({
     ggplot(t %>% filter(region != ''), aes(year, qq, group=region)) + 
       geom_line(size = 0.5, col='grey') +
       geom_line(data = t[t$region == input$region, ],
@@ -20,12 +34,49 @@ server <- function(input, output) {
       geom_text(data = labs[labs$region == input$region,], aes(2016, max, label=region), col='red', size=3) +
       labs(y = 'Cumulative number of earthquakes', x ='') + 
       scale_x_continuous(breaks= seq(1988, 2018, 4))
+    
+    
   })
-  
+
 
 }
 
 
 shinyApp(ui = ui, server = server)
+
+
+
+
+
+
+
+
+
+
+
+
+ui <- fluidPage(
+  selectInput("region", "Region", choices = unique(t$region)),
+  plotlyOutput("outplot"))
+
+server <- function(input, output) {
+  
+  output$outplot <- renderPlotly({
+    ggplot(t %>% filter(region != ''), aes(year, qq, group=region)) + 
+      geom_line(size = 0.5, col='grey') +
+      geom_line(data = t[t$region == input$region, ],
+                aes(year, qq), col='red') +
+      geom_text(data = labs[labs$region == input$region,], aes(2016, max, label=region), col='red', size=3) +
+      labs(y = 'Cumulative number of earthquakes', x ='') + 
+      scale_x_continuous(breaks= seq(1988, 2018, 4))
+    
+    
+  })
+}
+
+
+shinyApp(ui = ui, server = server)
+
+
 
 
